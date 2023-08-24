c1056c8be47e61efddcc11452adbc1c19db4ed2e
Ewan <ewd00010@outlook.com>
2023-08-24 15:08:12 +0100

    changed most docs,comments; removed pointless type safety; add tests function and assert include

--- math/extended_euclid_algorithm.cpp
+++ math/extended_euclid_algorithm.cpp
@@ -1,96 +1,112 @@
 /**
  * @file
- * @brief GCD using [extended Euclid's algorithm]
+ * @brief finds the Greatest common divisor using [extended Euclidean algorithm]
  * (https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm)
  *
+ * @details
  * Finding coefficients of a and b ie x and y in  Bézout's identity
  * \f[\text{gcd}(a, b) = a \times x + b \times y \f]
- * This is also used in finding Modular
- * multiplicative inverse of a number. (A * B)%M == 1 Here B is the MMI of A for
+ * This is also used in finding the Modular
+ * multiplicative inverse (MMI) of a number. \f[(A * B)%M == 1\f] Here B is the MMI of A for
  * given M, so extendedEuclid (A, M) gives B.
  */
+
 #include <algorithm>  // for swap function
-#include <iostream>
+#include <assert.h> // for assert
+#include <iostream> // for IO operations
 
 /**
- * function to update the coefficients per iteration
- * \f[r_0,\,r = r,\, r_0 - \text{quotient}\times r\f]
- *
- * @param[in,out] r signed or unsigned
- * @param[in,out] r0 signed or unsigned
- * @param[in] quotient  unsigned
+ * @brief Mathematical algorithms
+ * @namespace
  */
-template <typename T, typename T2>
-inline void update_step(T *r, T *r0, const T2 quotient) {
-    T temp = *r;
-    *r = *r0 - (quotient * temp);
-    *r0 = temp;
-}
+namespace math {
 
-/**
- * Implementation using iterative algorithm from
- * [Wikipedia](https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Pseudocode)
- *
- * @param[in] A unsigned
- * @param[in] B unsigned
- * @param[out] GCD unsigned
- * @param[out] x signed
- * @param[out] y signed
- */
-template <typename T1, typename T2>
-void extendedEuclid_1(T1 A, T1 B, T1 *GCD, T2 *x, T2 *y) {
-    if (B > A)
-        std::swap(A, B);  // Ensure that A >= B
-
-    T2 s = 0, s0 = 1;
-    T2 t = 1, t0 = 0;
-    T1 r = B, r0 = A;
-
-    while (r != 0) {
-        T1 quotient = r0 / r;
-        update_step(&r, &r0, quotient);
-        update_step(&s, &s0, quotient);
-        update_step(&t, &t0, quotient);
+    /**
+    * @brief Function to update the coefficients per iteration
+    * \f[r_0,\,r = r,\, r_0 - \text{quotient}\times r\f]
+    * @param r
+    * @param r0
+    * @param quotient
+    */
+    inline void update_step(u_int64_t *r, u_int64_t *r0, const u_int64_t quotient) {
+        u_int64_t temp = *r;
+        *r = *r0 - (quotient * temp);
+        *r0 = temp;
     }
-    *GCD = r0;
-    *x = s0;
-    *y = t0;
+
+    /**
+    * @brief Implementation using iterative algorithm from
+    * [Wikipedia](https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Pseudocode)
+    * @param A
+    * @param B
+    * @param GCD
+    * @param x
+    * @param y
+    */
+    void extendedEuclid_1(u_int64_t A, u_int64_t B, u_int64_t *GCD, int64_t*x, int64_t *y) {
+        if (B > A)
+            std::swap(A, B);  // Ensure that A >= B
+
+        u_int64_t s = 0, s0 = 1;
+        u_int64_t t = 1, t0 = 0;
+        u_int64_t r = B, r0 = A;
+
+        while (r != 0) {
+            u_int64_t quotient = r0 / r;
+            update_step(&r, &r0, quotient);
+            update_step(&s, &s0, quotient);
+            update_step(&t, &t0, quotient);
+        }
+        *GCD = r0;
+        *x = s0;
+        *y = t0;
+    }
+
+    /**
+    * @brief Implementation using recursive algorithm
+    * @param A
+    * @param B
+    * @param GCD
+    * @param x
+    * @param y
+    */
+    void extendedEuclid(u_int64_t A, u_int64_t B, u_int64_t *GCD, int64_t *x, int64_t *y) {
+        if (B > A)
+            std::swap(A, B);  // Ensure that A >= B
+
+        if (B == 0) {
+            *GCD = A;
+            *x = 1;
+            *y = 0;
+        } else {
+            extendedEuclid(B, A % B, GCD, x, y);
+            int64_t temp = *x;
+            *x = *y;
+            *y = temp - (A / B) * (*y);
+        }
+    }
+}  // namespace math
+
+/**
+ * @brief Self-test implementations
+ * @returns void
+ */
+static void tests(){
+    assert(NULL);
 }
 
 /**
- * Implementation using recursive algorithm
- *
- * @param[in] A unsigned
- * @param[in] B unsigned
- * @param[out] GCD unsigned
- * @param[in,out] x signed
- * @param[in,out] y signed
+ * @brief Main function
+ * @return 0 on exit
  */
-template <typename T, typename T2>
-void extendedEuclid(T A, T B, T *GCD, T2 *x, T2 *y) {
-    if (B > A)
-        std::swap(A, B);  // Ensure that A >= B
-
-    if (B == 0) {
-        *GCD = A;
-        *x = 1;
-        *y = 0;
-    } else {
-        extendedEuclid(B, A % B, GCD, x, y);
-        T2 temp = *x;
-        *x = *y;
-        *y = temp - (A / B) * (*y);
-    }
-}
-
-/// Main function
 int main() {
-    uint32_t a, b, gcd;
-    int32_t x, y;
+    tests();
+    u_int64_t a, b, gcd;
+    int64_t x, y;
     std::cin >> a >> b;
-    extendedEuclid(a, b, &gcd, &x, &y);
+    math::extendedEuclid(a, b, &gcd, &x, &y);
     std::cout << gcd << " " << x << " " << y << std::endl;
-    extendedEuclid_1(a, b, &gcd, &x, &y);
+    math::extendedEuclid_1(a, b, &gcd, &x, &y);
     std::cout << gcd << " " << x << " " << y << std::endl;
     return 0;
 }
